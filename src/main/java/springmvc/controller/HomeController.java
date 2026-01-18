6d1763536d8d361a1e96e11c3aeb19b40ab22abd
mayraverma <av7358385@gmail.com>
2026-01-18 22:51:34 +0530

    Update HomeController.java

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@Restcontroller
+
 @RequestMapping("/first")
 public class HomeController {
 	@RequestMapping("/home")
